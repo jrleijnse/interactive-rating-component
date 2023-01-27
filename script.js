1224ebb558b59d0eb11460f699dae2805974946e
Jeroen Leijnse <jr_leijnse@live.nl>
2023-01-27 11:43:53 +0100

    changes

---
+++ script.js
@@ -0,0 +1,16 @@
+const numberList = document.querySelector(".number-list");
+const selectedNumber = undefined;
+// addEventLi => list
+// e.target.dataset.rate
+// addC
+// store number => seleNum = 1
+
+const submitRating = function (e) {
+  // check if sNum != und
+  // === und => alerrt ... select a number
+
+// != undefined => submit() => reste()
+  // send the request
+};
+
+const selectRating = function ()
