982e14bc31f4411e0c35bb626ebcac58a381b89b
Jeroen Leijnse <jr_leijnse@live.nl>
2023-01-28 12:45:02 +0100

    add logic

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const numberList = document.querySelector(".number-list");
-const selectedNumber = undefined;
-// addEventLi => list
-// e.target.dataset.rate
-// addC
-// store number => seleNum = 1
+const btnSubmit = document.querySelector(".btn-submit");
+const card = document.querySelector(".card-container");
+const thanksCard = document.querySelector(".thanks-container");
 
-const submitRating = function (e) {
-  // check if sNum != und
-  // === und => alerrt ... select a number
+let selectedNumber = undefined;
 
-// != undefined => submit() => reste()
-  // send the request
-};
+numberList.addEventListener("click", function (e) {
+  const number = e.target;
+  selectedNumber = e.target.getAttribute("data-rating");
 
-const selectRating = function ()
+  number.parentElement
+    .querySelectorAll(".active-number")
+    .forEach((e) => e.classList.remove("active-number"));
+
+  number.classList.add("active-number");
+  numberList.classList.remove("active-number");
+});
+
+btnSubmit.addEventListener("click", function () {
+  if (!selectedNumber) alert("Please select a number");
+  if (selectedNumber) {
+    card.classList.add("hidden");
+    thanksCard.classList.remove("hidden");
+  }
+});
