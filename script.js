478419281556668f5abd523de73d6349f5a7d513
Jeroen Leijnse <jr_leijnse@live.nl>
2023-01-29 12:36:47 +0100

    add comments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const numberList = document.querySelector(".number-list");
+const numbers = document.querySelectorAll(".number");
+const activeNumber = document.querySelector(".active-number");
 const btnSubmit = document.querySelector(".btn-submit");
 const card = document.querySelector(".card-container");
 const thanksCard = document.querySelector(".thanks-container");
@@ -6,26 +7,30 @@ const thanksRating = document.querySelector(".thanks-rating");
 
 let selectedNumber = undefined;
 
-// Remove active number state when clicking somewhere on the page
-document.addEventListener("click", function (event) {
-  if (!event.target.matches(".number")) {
-    numberList
-      .querySelectorAll(".active-number")
-      .forEach((e) => e.classList.remove("active-number"));
-    selectedNumber = undefined;
-  }
+// Add active number state when clicking on a number
+numbers.forEach((number) => {
+  number.addEventListener("click", function (e) {
+    // Track number to display in thank you form
+    selectedNumber = e.target.getAttribute("data-rating");
+
+    // Toggle active number state
+    if (!activeNumber) {
+      e.target.classList.toggle("active-number");
+    }
+
+    // Remove active state from last selected number and add active to new selected number
+    if (e.target.classList.contains("active-number")) {
+      numbers.forEach((e) => e.classList.remove("active-number"));
+      number.classList.add("active-number");
+    }
+  });
 });
 
-// Add active number state when clicking on a number
-numberList.addEventListener("click", function (e) {
-  selectedNumber = e.target.getAttribute("data-rating");
-  const number = e.target;
-
-  if (e.target.classList.contains("number")) {
-    numberList
-      .querySelectorAll(".active-number")
-      .forEach((e) => e.classList.remove("active-number"));
-    number.classList.add("active-number");
+// Remove active number state when clicking somewhere on the page
+document.addEventListener("click", function (e) {
+  if (!e.target.matches(".number")) {
+    numbers.forEach((e) => e.classList.remove("active-number"));
+    selectedNumber = undefined;
   }
 });
 
@@ -37,5 +42,6 @@ btnSubmit.addEventListener("click", function () {
     thanksCard.classList.remove("hidden");
   }
 
+  // Display selected rating in thank youcard
   thanksRating.textContent = `You selected ${selectedNumber} out of 5`;
 });
